c45c9af4b898cec2e4ce2383f0d10e9f4b698b82
Tiaa User <Tiaa User@LAPTOP-KQSSRBE5>
2020-06-16 03:39:28 +0530

    changes in object initialization values

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -5,13 +5,13 @@ public class BinarySearchTree {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		TreeNode root = new TreeNode(1);
-		root.left = new TreeNode(2);
-		root.right = new TreeNode(3);
-		root.left.left = new TreeNode(4);
-		root.left.right = new TreeNode(5);
-		root.right.left = new TreeNode(6);
-		root.right.right = new TreeNode(7);
+		TreeNode root = new TreeNode(4);
+		root.left = new TreeNode(3);
+		root.right = new TreeNode(2);
+		root.left.left = new TreeNode(1);
+		root.left.right = new TreeNode(0);
+		root.right.left = new TreeNode(5);
+		root.right.right = new TreeNode(6);
 
 		BinarySearchTree binarySearchTree = new BinarySearchTree();
 		TreeNode  treeNode = binarySearchTree.searchBST(root, 5);
